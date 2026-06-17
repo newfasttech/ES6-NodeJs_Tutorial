c7abbae39cd74c7996e200f556b7daf929552cde
newfasttech <168957349+newfasttech@users.noreply.github.com>
2026-06-18 00:18:05 +0530

    Refactor try-catch example in trycatch.js
    Updated try-catch example to demonstrate error handling and program flow.

--- ES6 Js/trycatch.js
+++ ES6 Js/trycatch.js
@@ -1,35 +1,40 @@
-function withdrawMoney(amount) {
-    let balance = 5000; 
-
-    console.log("Processing withdrawal of $" + amount + "...");
-
-    if (amount > balance) {
-        throw new Error("Insufficient Balance! You cannot withdraw more than $" + balance);
-    }
-
-    console.log("Success! Please collect your cash: $" + amount);
-}
+console.log("Program started...");
 
 try {
-    withdrawMoney(7000); 
+    // 1. JavaScript will 'Try' to execute this code
+    downloadData(); // Supposing this function does not exist (Causes an Error)
+    
 } catch (error) {
-    console.log("🚨 Bank Alert: " + error.message);
+    // 2. If any error occurs above, instead of crashing, the code directly jumps here
+    console.log(" Unexpected Error Occurred: " + error.message);
 }
 
-console.log("Thank you for using our ATM.");
+// 3. See the magic! This line runs successfully and the program does not stop.
+console.log("Program did not crash. This important next line executed successfully!");
 
 
 
-// console.log("Program started...");
 
-// try {
-//     // 1. JavaScript will 'Try' to execute this code
-//     downloadData(); // Supposing this function does not exist (Causes an Error)
-    
-// } catch (error) {
-//     // 2. If any error occurs above, instead of crashing, the code directly jumps here
-//     console.log(" Unexpected Error Occurred: " + error.message);
+
+// function withdrawMoney(amount) {
+//     let balance = 5000; 
+
+//     console.log("Processing withdrawal of $" + amount + "...");
+
+//     if (amount > balance) {
+//         throw new Error("Insufficient Balance! You cannot withdraw more than $" + balance);
+//     }
+
+//     console.log("Success! Please collect your cash: $" + amount);
 // }
 
-// // 3. See the magic! This line runs successfully and the program does not stop.
-// console.log("Program did not crash. This important next line executed successfully!");
+// try {
+//     withdrawMoney(7000); 
+// } catch (error) {
+//     console.log(" Bank Alert: " + error.message);
+// }
+
+// console.log("Thank you for using our ATM.");
+
+
+
