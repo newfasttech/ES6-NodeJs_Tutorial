18ac0f1f29e6ab8706e4cd404053122bedc0ad92
GohelHiren <hirengohel5173@gmail.com>
2026-06-18 19:36:33 +0530

    add module topic

---
+++ module-node/ES6-Module/index.js
@@ -0,0 +1,4 @@
+import {sum,sub} from './calculate.js'
+
+console.log(sum(45,45));
+console.log(sub(4,45));
---
+++ module-node/commonJS-Module/calc.js
@@ -0,0 +1,4 @@
+const cal = require('./calculate')
+
+console.log(cal.sum(25,50));
+console.log(cal.sub(25,5));
